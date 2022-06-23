67db097896b4eea68ede247b45412cc581119eb7
Svyat <jesus@Svyats-MacBook-Pro.local>
2022-06-23 13:47:00 +0400

    Inner UserService method removed

--- application/services.py
+++ application/services.py
@@ -1,11 +1,8 @@
 import logging
-from datetime import datetime, timezone
-
-from typing import Optional
+from datetime import datetime
 
 from aiogram.types import Message
 
-from models import User
 from application.serializers import MessageDataSerializer
 from application.repositories import UserRepository
 from application.exceptions import TooManyAttemptsError
@@ -20,16 +17,11 @@ class UserService:
     def __init__(self, user_repository: UserRepository):
         self._user_repository: UserRepository = user_repository
 
-    async def _get_user(self, message: Message) -> Optional[User]:
-        """TODO: get rid of inner method
-        in order not to generate redundancy"""
+    async def update_user_uploaded_date(self, message: Message) -> None:
         user_data = MessageDataSerializer.parse_user_data(message=message)
-        return await self._user_repository.get_record(
+        user = await self._user_repository.get_record(
             telegram_user_id=user_data.telegram_user_id
         )
-
-    async def update_user_uploaded_date(self, message: Message) -> None:
-        user = await self._get_user(message)
         now = datetime.now()
         if (user.last_uploaded is not None) and (
             (now - user.last_uploaded).days < 1
@@ -39,7 +31,9 @@ class UserService:
 
     async def create_user_if_none(self, message: Message) -> None:
         user_data = MessageDataSerializer.parse_user_data(message=message)
-        user = await self._get_user(message)
+        user = await self._user_repository.get_record(
+            telegram_user_id=user_data.telegram_user_id
+        )
         if user is None:
             await self._user_repository.create_record(
                 telegram_user_id=user_data.telegram_user_id,
