55fa161c447e519723c1ca6e2ebcc0644c37497b
Svyat <jesus@Svyats-MacBook-Pro.local>
2022-06-24 11:38:37 +0400

    Handlers _user_service --> user_service naming fix

--- application/handlers.py
+++ application/handlers.py
@@ -12,16 +12,16 @@ from application.exceptions import TooManyAttemptsError
 
 
 @dispatcher.message_handler(commands=["start", "help"])
-async def register_user(message: Message, _user_service=Gateways.user_service) -> None:
-    await _user_service.create_user_if_none(message=message)
+async def register_user(message: Message, user_service=Gateways.user_service) -> None:
+    await user_service.create_user_if_none(message=message)
     await message.reply(WELCOME)
 
 
 # Handlers order is important since dispatcher should check for document type first
 @dispatcher.message_handler(content_types=ContentTypes.DOCUMENT)
-async def add_file_to_queue(message: Message, _user_service=Gateways.user_service):
+async def add_file_to_queue(message: Message, user_service=Gateways.user_service):
     try:
-        await _user_service.update_user_uploaded_date(message)
+        await user_service.update_user_uploaded_date(message)
         await message.answer(FILE_ADDED_TO_QUEUE)
     except TooManyAttemptsError:
         await message.answer(TOO_MANY_ATTEMPTIONS)
