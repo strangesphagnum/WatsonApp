27df87cff0e51fb675c13da4276999ade3fd22be
Svyat <jesus@Svyats-MacBook-Pro.local>
2022-06-24 17:02:38 +0400

    f//migrations/env.py import

--- migrations/env.py
+++ migrations/env.py
@@ -5,7 +5,7 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 from sqlalchemy.ext.asyncio import AsyncEngine
 
-from models import Base
+from application.models import Base
 from settings import settings
 
 config = context.config
